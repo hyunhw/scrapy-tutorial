8ca3e3467606524116cbc60f829d4b66309df186
Jennifer Hwang <hhwang0908@gmail.com>
2018-04-30 00:02:24 -0400

    working

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -12,7 +12,6 @@ class QuotesSpider (scrapy.Spider):
     # start_requests can be replaced by defining start_urls
     """
     def start_requests(self):
-        """Return an iterable of Requests"""
         urls = [
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/',
@@ -23,9 +22,18 @@ class QuotesSpider (scrapy.Spider):
 
     def parse(self, response):
         """method that handles response downloaded for each request"""
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").extract_first(),
+                'author': quote.css("small.author::text").extract_first(),
+                'tags': quote.css("div.tags a.tag::text").extract()
+            }
+        
+        """
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+        """
 
