610f956d4899e2c033f9ef5e10b118f67509504f
Jennifer Hwang <hhwang0908@gmail.com>
2018-04-29 23:45:07 -0400

    working

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -4,7 +4,13 @@ class QuotesSpider (scrapy.Spider):
     # identify the spider
     #  must be unique within a project
     name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
+    # start_requests can be replaced by defining start_urls
+    """
     def start_requests(self):
         """Return an iterable of Requests"""
         urls = [
@@ -13,6 +19,7 @@ class QuotesSpider (scrapy.Spider):
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
+    """
 
     def parse(self, response):
         """method that handles response downloaded for each request"""
